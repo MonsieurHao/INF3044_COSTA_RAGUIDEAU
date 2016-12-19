f7b4477b425f20c14dda323543c2739359c24c15
Akiya <akiyaraguideau@yahoo.fr>
2016-12-19 07:59:26 +0100

    presque fini

--- app/src/main/java/org/esiea/costa_raguideau/projetmobile_costa_raguideau/PageAccueil.java
+++ app/src/main/java/org/esiea/costa_raguideau/projetmobile_costa_raguideau/PageAccueil.java
@@ -1,9 +1,16 @@
 
 package org.esiea.costa_raguideau.projetmobile_costa_raguideau;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -13,7 +20,7 @@ public class PageAccueil extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_page_accueil);
+        setContentView(R.layout.activity_page_acceuil);
         TextView view = (TextView) findViewById(R.id.Intro);
         view.setText(R.string.Intro);
         view = (TextView) findViewById(R.id.BoutonVie);
@@ -22,6 +29,15 @@ public class PageAccueil extends AppCompatActivity {
         view.setText(R.string.Avis);
         view = (TextView) findViewById(R.id.Next);
         view.setText(R.string.Next);
+        this.setTitle(R.string.titre_acceuil);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
     public void petite_soif (View view){
@@ -31,5 +47,44 @@ public class PageAccueil extends AppCompatActivity {
 
     public void arrogant(View v) {
         Toast.makeText(getApplicationContext(), getString(R.string.Arrogant), Toast.LENGTH_LONG).show();
+    }
+
+    public void video(View v) {
+
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=idF0xEMiOvI")));
+        Log.i("Video", "Video Playing....");
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Donation:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.dialog_message);
+                builder.setTitle(R.string.dialog_title);
+                builder.setPositiveButton(R.string.ok1, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.setNeutralButton(R.string.ok3, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton(R.string.ok2, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                builder.show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
--- app/src/main/java/org/esiea/costa_raguideau/projetmobile_costa_raguideau/Petite_soif.java
+++ app/src/main/java/org/esiea/costa_raguideau/projetmobile_costa_raguideau/Petite_soif.java
@@ -3,9 +3,11 @@ package org.esiea.costa_raguideau.projetmobile_costa_raguideau;
 import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
@@ -14,9 +16,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,6 +45,7 @@ public class Petite_soif extends AppCompatActivity {
         setContentView(R.layout.activity_petite_soif);
         TextView view = (TextView) findViewById(R.id.titre);
         view.setText(R.string.titre);
+        this.setTitle(R.string.titre_recycler);
         telecharge_bieres.debut_plus_de_bieres(this);
         IntentFilter intentFilter = new IntentFilter(maj_biere);
         LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(), intentFilter);
@@ -46,18 +54,58 @@ public class Petite_soif extends AppCompatActivity {
         rv.setAdapter(new ElementAdapter(getElementsFromFile()));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Donation:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(R.string.dialog_message);
+                builder.setTitle(R.string.dialog_title);
+                builder.setPositiveButton(R.string.ok1, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                builder.setNeutralButton(R.string.ok3, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+                builder.setNegativeButton(R.string.ok2, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getApplicationContext(), getString(R.string.merci), Toast.LENGTH_LONG).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                builder.show();
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
+
     public class BierUpdate extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent){
             Log.i("BierUpdate",intent.getAction());
-            notification_test();
+            notification();
             ElementAdapter adapter = (ElementAdapter) rv.getAdapter();
             adapter.setNewElement(getElementsFromFile());
         }
     }
 
-    public void notification_test(){
-        NotificationCompat.Builder notif_t =(NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext()).setSmallIcon(R.mipmap.ic_launcher).setContentText("biere updated").setContentTitle("download");
+    public void notification(){
+        NotificationCompat.Builder notif_t =(NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext()).setSmallIcon(R.drawable.ic_stat_name).setContentText(this.getString(R.string.downloaded)).setContentTitle("download");
         NotificationManager nm=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         nm.notify(0,notif_t.build());
     }
